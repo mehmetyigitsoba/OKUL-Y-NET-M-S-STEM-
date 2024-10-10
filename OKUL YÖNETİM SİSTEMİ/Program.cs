164b1e3f8271318ad6dc5486f59849dd5dffffee
YİĞİT SOBA <mehmetyigitsoba@hotmail.com>
2024-10-10 21:44:15 +0300

    Add project files.

---
+++ OKUL YÖNETİM SİSTEMİ/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace OKUL_YÖNETİM_SİSTEMİ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            VERİLER.EKLE();
+
+            VERİLER.NOTLAR();
+            VERİLER.LISTELEME();
+        }
+    }
+}
---
+++ OKUL YÖNETİM SİSTEMİ/nesne.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OKUL_YÖNETİM_SİSTEMİ
+{ 
+    namespace nesne
+    {
+        public class ogretmenler
+        {
+                public string ogretmenadi;
+        public string ogretmensoyadi;
+        public int ogretmenyasi;
+
+
+    }
+        public class dersler
+        {
+            public int dersinSınıfı;
+            public string ders;
+            public string dersinHocası;
+            public string []dersinOgrencileri;
+
+
+            public class sınıflar
+        {
+            public int sınıf;
+        }
+       
+            
+           
+            public class ogrenciler
+            {
+                public string ogrenciadi;
+                public string ogrencisoyadi;
+                public int ogrenciyasi;
+                public int ogrencinotu;
+
+
+
+
+            }
+        }
+        
+
+    }
+   
+}
